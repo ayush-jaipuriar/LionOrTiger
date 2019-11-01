014ffad476219cc41c3652a5a2a294068fa6839a
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-01 23:59:58 +0530

    "Made the alpha(0.2) for all imageViews. Created an enum named Player with values ONE and TWO to represent the currentPlayer, Player One on click will generate a lion drawable whereas Player Two on click will generate a Tiger drawable, both the times the control will shift to the other player"

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    enum Player {
+        ONE, TWO;
+    }
+
+    Player currentPlayer = Player.ONE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +25,14 @@ public class MainActivity extends AppCompatActivity {
         ImageView tappedImageView = (ImageView) view;
 
         tappedImageView.setTranslationX(-2000);
-        tappedImageView.setImageResource(R.drawable.lion);
+        if (currentPlayer==Player.ONE) {
+            tappedImageView.setImageResource(R.drawable.lion);
+            currentPlayer = Player.TWO;
+        } else if ( currentPlayer == Player.TWO) {
+            tappedImageView.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.ONE;
+        }
+
         tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(500);
     }
 
