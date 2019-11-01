e14c4ac4ac59773af13fae9cf4f972f383c4ffe5
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-02 00:27:39 +0530

    "Created a two dimensional array called which contains all the possible permuations for the winner to be decided. Yet to implement the two dimensional array into the imageViewIsTapped method"

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     Player currentPlayer = Player.ONE;
     Player[] playerChoices =new Player[9];
+    int [][] winnerRowColumns = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},
+            {1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
