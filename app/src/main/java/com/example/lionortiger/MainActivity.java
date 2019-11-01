ac4f94e64d4812a57f8ca11c716c71ca55e9881e
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-01 23:31:27 +0530

    "For all imageViews made alpha(0). Then removed the lion image from the all the imageViews, and created a onClick method for them and once a particular row/column is called now, a nifty animation is shown and an image of a lion gets set on that particular row/column of the image view"

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void imageViewIsClicked (View view) {
+
+        ImageView tappedImageView = (ImageView) view;
+
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.lion);
+        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(500);
+    }
+
 }
