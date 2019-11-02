5e52fd46d4494f269b323ccdf94a81bf24798f91
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-02 10:56:34 +0530

    "Changed the background of the UI elements, added a new button to reset the game once the game gets finished, the button's visibility is initially set to Gone but once the boolean value of gameOver becomes true its visibility gets set to Visible and on button press the contents and variables get reset to their original values"

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.lionortiger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -12,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     enum Player {
         ONE, TWO, NONE;
     }
-
+    GridLayout gridLayout;
+    Button button;
     Player currentPlayer = Player.ONE;
     Player[] playerChoices =new Player[9];
     int [][] winnerRowColumns = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},
@@ -25,16 +29,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        gridLayout = findViewById(R.id.gridLayout);
+
 
         for (int i=0; i<9; i++) {
             playerChoices[i] = Player.NONE;
         }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTheGame();
+            }
+        });
     }
+
+
 
     public void imageViewIsClicked (View view) {
 
         ImageView tappedImageView = (ImageView) view;
         int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+
 
         if (playerChoices[tiTag] == Player.NONE && gameOver== false) {
             tappedImageView.setTranslationX(-2000);
@@ -49,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(500);
+            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
             Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
             for (int[] winnerColums : winnerRowColumns) {
                 if (playerChoices[winnerColums[0]] == playerChoices[winnerColums[1]]
@@ -65,9 +83,28 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "The winner of the game is : "+winner + "",
                             Toast.LENGTH_SHORT).show();
                     gameOver=true;
+                    button.setVisibility(View.VISIBLE);
                 }
             }
         }
+    }
+
+    void resetTheGame() {
+
+
+        for (int index=0; index< gridLayout.getChildCount(); index++) {
+
+            ImageView imageView = (ImageView) gridLayout.getChildAt(index);
+            imageView.setImageDrawable(null);
+            imageView.setAlpha(0.2f);
+        }
+        currentPlayer = Player.ONE;
+        gameOver = false;
+        for (int i=0; i<9; i++) {
+            playerChoices[i] = Player.NONE;
+        }
+
+
     }
 
 }
